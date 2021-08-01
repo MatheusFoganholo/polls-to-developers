7e5bbf00bd3bcd7cf398acef292ad7075d0638f3
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-08-01 11:23:15 -0300

    feat: add app router

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -3,4 +3,5 @@ export { default as LoginHeader } from './login-header/login-header';
 export * from './input/input';
 export * from './form-status/form-status';
 export * from './logo/logo';
+export * from './router/router';
 export * from './spinner/spinner';
