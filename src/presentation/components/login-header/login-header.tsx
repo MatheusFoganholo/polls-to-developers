84de430b313e5d5a64479d89b37beb1a58b7a5ae
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-08-01 10:46:55 -0300

    refactor: move header and footer to separate files

---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from 'react';
+import { Logo } from '@/presentation/components/logo/logo';
+import Styles from './login-header-styles.scss';
+
+const LoginHeader: React.FC = () => {
+  return (
+    <div>
+      <header className={Styles.header}>
+        <Logo />
+        <h1>4Dev - Polls To Developers</h1>
+      </header>
+    </div>
+  );
+};
+
+export default memo(LoginHeader);
