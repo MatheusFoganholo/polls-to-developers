729d0b221d1b2eea95da6a55e52531974950e650
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-08-03 10:59:18 -0300

    test: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidFieldError } from '@/validation/errors';
+import { MinLengthValidation } from './min-length-validation';
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is less than 6 characters', () => {
+    const sut = new MinLengthValidation('field', 6);
+    const error = sut.validate('123');
+
+    expect(error).toEqual(new InvalidFieldError());
+  });
+});
