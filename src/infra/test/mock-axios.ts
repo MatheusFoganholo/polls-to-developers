41d4b21963f3d1f14ebbab3b75f979485dfc5223
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-08-10 10:36:37 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { random } from 'faker';
 
+export const mockHttpResponse = (): any => ({
+  data: random.objectElement(),
+  status: random.number()
+});
+
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-  mockedAxios.post.mockResolvedValue({
-    data: random.objectElement(),
-    status: random.number()
-  });
+  mockedAxios.post.mockResolvedValue(mockHttpResponse());
 
   return mockedAxios;
 };
