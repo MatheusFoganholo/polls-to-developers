63d9898d899dd0a77c1fb74014e2650de2d0d9e3
Matheus Foganholo <matheusfoganholo@hotmail.com>
2021-08-11 18:25:00 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/presentation/pages/sign-up/sign-up.spec.tsx
+++ src/presentation/pages/sign-up/sign-up.spec.tsx
@@ -177,4 +177,13 @@ describe('SignUp Component', () => {
 
     expect(addAccountSpy.callsCount).toBe(1);
   });
+
+  test('Should not call AddAccount if form is invalid', async () => {
+    const validationError = random.words();
+    const { sut, addAccountSpy } = makeSut({ validationError });
+
+    await simulateValidSubmit(sut);
+
+    expect(addAccountSpy.callsCount).toBe(0);
+  });
 });
